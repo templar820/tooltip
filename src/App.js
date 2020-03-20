d763f3413f984ae7a4de087bc4fe30332bdf0eed
Кузнецов Иван Дмитриевич <41190483+templar820@users.noreply.github.com>
2020-03-20 18:29:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Tooltip extends React.Component{
+
+
+
+  constructor(props){
+    super(props)
+      this.state={
+        isRender: false,
+      }
+    }
+
+  Move(e) {
+    let element = e.target;
+    var tooltip = document.getElementById("Tooltip");
+    if(element.getAttribute("data-tooltip")){
+      let text = element.getAttribute("data-tooltip");
+      tooltip.textContent = text
+      
+      console.log(tooltip.clientWidth)
+      tooltip.style.position = "absolute";
+      tooltip.style.left = e.clientX - tooltip.clientWidth/2 + "px";
+      tooltip.style.top = e.clientY+ tooltip.clientHeight + "px";
+    }else{
+      tooltip.textContent = ""
+    }
+  }
+
+  render(){
+    return (
+      <div data-tooltip = {"trusssssssssssssssse"} style={{width:"1000px", height:"1000px", background: "grey"}} onMouseMove={(e) => this.Move(e)}>
+        <p>Привет как дела</p>
+        <div id="Tooltip" className="Tooltip">
+          <div></div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
-export default App;
+
