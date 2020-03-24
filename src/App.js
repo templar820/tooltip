230803d78a347172709716917588fc9fbaa28be1
Кузнецов Иван Дмитриевич <41190483+templar820@users.noreply.github.com>
2020-03-24 08:32:39 +0300

    before merge

--- src/App.js
+++ src/App.js
@@ -9,30 +9,44 @@ export default class Tooltip extends React.Component{
     super(props)
       this.state={
         isRender: false,
+        scrollX: 0,
+        scrollY: 0,
       }
     }
 
   Move(e) {
+    // console.log(e)
+    // console.log(e.screenY)
+    // console.log(e.clientY)
     let element = e.target;
     var tooltip = document.getElementById("Tooltip");
     if(element.getAttribute("data-tooltip")){
       let text = element.getAttribute("data-tooltip");
       tooltip.textContent = text
-      
-      console.log(tooltip.clientWidth)
       tooltip.style.position = "absolute";
+      // this.state.scrollX 
+      console.log(this.state.scrollY)
       tooltip.style.left = e.clientX - tooltip.clientWidth/2 + "px";
-      tooltip.style.top = e.clientY+ tooltip.clientHeight + "px";
+      tooltip.style.top  = e.clientY + tooltip.clientHeight + "px";
     }else{
       tooltip.textContent = ""
     }
   }
 
+  Scroll(e){
+    let x = this.state.scrollX;
+    x+=e.deltaX;
+    console.log(e.deltaX)
+    let y = this.state.scrollY;
+    y+=e.deltaY;
+    this.setState({scrollX:x, scrollY:y})
+  }
+
   render(){
     return (
-      <div data-tooltip = {"trusssssssssssssssse"} style={{width:"1000px", height:"1000px", background: "grey"}} onMouseMove={(e) => this.Move(e)}>
+      <div data-tooltip = {"trusssssssssssssssse"} style={{width:"1000px", height:"1000px", background: "grey"}} onMouseMove={(e) => this.Move(e)} onWheel = {(e) => this.Scroll(e)}>
         <p>Привет как дела</p>
-        <div id="Tooltip" className="Tooltip">
+        <div id="Tooltip" onMouseMove={()=>{}} className="Tooltip">
           <div></div>
         </div>
       </div>
